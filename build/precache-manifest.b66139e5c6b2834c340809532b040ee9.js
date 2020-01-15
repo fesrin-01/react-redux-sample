f459c70ee8c8a3b68c9cf85dc35e0238b07b5a4a
fesrin-01 <fesrin01@gmail.com>
2020-01-15 14:10:27 +0900

    add: public

---
+++ build/precache-manifest.b66139e5c6b2834c340809532b040ee9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0e729fb73450441ace5020b2095e9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4502d0697f8f256e57cd",
+    "url": "/static/js/2.f0a32f0c.chunk.js"
+  },
+  {
+    "revision": "94059898a7eb442dcec1",
+    "url": "/static/js/main.c5e538a1.chunk.js"
+  },
+  {
+    "revision": "f2752dc21d01a25b128a",
+    "url": "/static/js/runtime-main.5d4369db.js"
+  }
+]);
